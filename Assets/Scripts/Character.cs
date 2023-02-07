301ec2f50f4158054e85302897dba40c62c7f2f2
Adam Mendoza <adammendoza27@gmail.com>
2023-02-07 11:00:49 -0800

    FightCLub

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -7,9 +7,42 @@ public class Character : MonoBehaviour
     public enum CharacterType {Player, Enemy, Object, Troop,};
 
     public float speed;
-    public int health;
-    private int maxHealth;
+    public float currentHealth;
+    public float maxHealth;
 
     public CharacterType Type;
 
+    void Start()
+    {
+        currentHealth = maxHealth;
+        //healthBar.SetMaxHealth(maxHealth);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TakeDamage(2);
+        }
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        if (currentHealth < 0)
+        {
+            currentHealth = 0;
+        }
+        //healthBar.SetHealth(currentHealth);
+
+    }
+    public void Heal(int health)
+    {
+        currentHealth += health;
+        if (currentHealth > maxHealth)
+        {
+            currentHealth = maxHealth;
+        }
+        //healthBar.SetHealth(currentHealth);
+    }
 }
