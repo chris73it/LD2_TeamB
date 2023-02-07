9366d0a18f94ddca1c0f987c33d7c5524a6b7024
Adam Mendoza <adammendoza27@gmail.com>
2023-02-07 12:12:33 -0800

    PUSH

---
+++ Assets/Scripts/EnemyAttack.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+    public MeleeEnemies chase;
+    public Melee melee;
+    public int damage;
+    public float attackRate;
+    public GameObject weapon, player;
+    public Transform weaponTransform;
+
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+        melee.damage = damage;
+        melee.rate = attackRate;
+    }
+
+    void Update()
+    {
+        Animation();
+
+        if (weapon != null)
+        {
+            if (chase.distance < 4)
+            {
+//                weapon.GetComponent<Melee>().enable();
+            }
+            else
+            {
+//                weapon.GetComponent<Melee>().disable();
+            }
+        }
+    }
+
+    void Animation()
+    {
+        Vector2 dir = (player.transform.position - transform.position).normalized;
+        float angle = -1 * Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg; //find angle in degrees from enemy to player
+        weaponTransform.rotation = Quaternion.AngleAxis(angle, Vector3.back);
+    }
+}
