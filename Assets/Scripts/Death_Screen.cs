0f38aabab0bc4d0bcdcf40163197ec6e7f6b8d0e
Adam Mendoza <adammendoza27@gmail.com>
2023-02-16 11:24:15 -0800

    Revert "Revert "stuff""
    This reverts commit 2d5cbb5e62c512d42917866bd818e601f483372c.

---
+++ Assets/Scripts/Death_Screen.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Death_Screen : MonoBehaviour
+{
+    public Canvas Death;
+    private Character Health;
+    private float health;
+    private void Start()
+    {
+        Health = GetComponent<Character>();
+    }
+    private void Update()
+    {
+        health = Health.currentHealth;
+        death();
+    }
+    private void death()
+    {
+        if (health == 0)
+        {
+            Death.gameObject.SetActive(true);
+        }
+    }
+}
