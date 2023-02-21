629474be12b4b0a206aa46169a469d08710037e4
Adam Mendoza <adammendoza27@gmail.com>
2023-02-21 11:52:47 -0800

    stuff

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public List<GameObject> enemyList = new List<GameObject>();
+    public float spawnRangeXMin;
+    public float spawnRangeXMax;
+    public float spawnRangeYMin;
+    public float spawnRangeYMax;
+    public int numToSpawnPerInterval = 5;
+    public int SpawnCd;
+
     void Start()
     {
-        
+        StartCoroutine(spawnEnemy(SpawnCd, enemyList));
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator spawnEnemy(float interval, List<GameObject> enemy)
     {
-        
+        for (int i = 0; i <= numToSpawnPerInterval; i++)
+        {
+            int selectEnemy = Random.Range(0, enemyList.Count - 1);
+            Vector3 v3Pos = Camera.main.ViewportToWorldPoint(new Vector3(Random.Range(spawnRangeXMin, spawnRangeXMax), Random.Range(spawnRangeYMin, spawnRangeYMax), 0.5f));
+            v3Pos.z = -0.2f;
+            GameObject spawn = Instantiate(enemy[selectEnemy], v3Pos, Quaternion.identity);
+
+        }
+        yield return new WaitForSeconds(2f);
+        yield return new WaitForSeconds(interval);
+        StartCoroutine(spawnEnemy(interval, enemy));
     }
 }
