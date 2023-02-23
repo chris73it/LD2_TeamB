266da9b5d8fb626be18e358f5abb1c1f0f22fd77
Adam Mendoza <adammendoza27@gmail.com>
2023-02-23 11:56:40 -0800

    ugh bungo

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -4,31 +4,139 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    public List<GameObject> enemyList = new List<GameObject>();
-    public float spawnRangeXMin;
-    public float spawnRangeXMax;
-    public float spawnRangeYMin;
-    public float spawnRangeYMax;
-    public int numToSpawnPerInterval = 5;
-    public int SpawnCd;
+    public List<Enemy> enemies = new List<Enemy>();
+    public int currWave;
+    private int waveValue;
+    public List<GameObject> enemiesToSpawn = new List<GameObject>();
 
+    public Transform[] spawnLocation;
+    public int spawnIndex;
+
+    public int waveDuration;
+    private float waveTimer;
+    private float spawnInterval;
+    private float spawnTimer;
+
+    public List<GameObject> spawnedEnemies = new List<GameObject>();
+    // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(spawnEnemy(SpawnCd, enemyList));
+        GenerateWave();
     }
 
-    private IEnumerator spawnEnemy(float interval, List<GameObject> enemy)
+    // Update is called once per frame
+    void FixedUpdate()
     {
-        for (int i = 0; i <= numToSpawnPerInterval; i++)
+        if (spawnTimer <= 0)
         {
-            int selectEnemy = Random.Range(0, enemyList.Count - 1);
-            Vector3 v3Pos = Camera.main.ViewportToWorldPoint(new Vector3(Random.Range(spawnRangeXMin, spawnRangeXMax), Random.Range(spawnRangeYMin, spawnRangeYMax), 0.5f));
-            v3Pos.z = -0.2f;
-            GameObject spawn = Instantiate(enemy[selectEnemy], v3Pos, Quaternion.identity);
+            //spawn an enemy
+            if (enemiesToSpawn.Count > 0)
+            {
+                GameObject enemy = (GameObject)Instantiate(enemiesToSpawn[0], spawnLocation[spawnIndex].position, Quaternion.identity); // spawn first enemy in our list
+                enemiesToSpawn.RemoveAt(0); // and remove it
+                spawnedEnemies.Add(enemy);
+                spawnTimer = spawnInterval;
 
+                if (spawnIndex + 1 <= spawnLocation.Length - 1)
+                {
+                    spawnIndex++;
+                }
+                else
+                {
+                    spawnIndex = 0;
+                }
+            }
+            else
+            {
+                waveTimer = 0; // if no enemies remain, end wave
+            }
         }
-        yield return new WaitForSeconds(2f);
-        yield return new WaitForSeconds(interval);
-        StartCoroutine(spawnEnemy(interval, enemy));
+        else
+        {
+            spawnTimer -= Time.fixedDeltaTime;
+            waveTimer -= Time.fixedDeltaTime;
+        }
+
+        if (waveTimer <= 0 && spawnedEnemies.Count <= 0)
+        {
+            currWave++;
+            GenerateWave();
+        }
     }
+
+    public void GenerateWave()
+    {
+        waveValue = currWave * 10;
+        GenerateEnemies();
+
+        spawnInterval = waveDuration / enemiesToSpawn.Count; // gives a fixed time between each enemies
+        waveTimer = waveDuration; // wave duration is read only
+    }
+
+    public void GenerateEnemies()
+    {
+        // Create a temporary list of enemies to generate
+        // 
+        // in a loop grab a random enemy 
+        // see if we can afford it
+        // if we can, add it to our list, and deduct the cost.
+
+        // repeat... 
+
+        //  -> if we have no points left, leave the loop
+
+        List<GameObject> generatedEnemies = new List<GameObject>();
+        while (waveValue > 0 || generatedEnemies.Count < 50)
+        {
+            int randEnemyId = Random.Range(0, enemies.Count);
+            int randEnemyCost = enemies[randEnemyId].cost;
+
+            if (waveValue - randEnemyCost >= 0)
+            {
+                generatedEnemies.Add(enemies[randEnemyId].enemyPrefab);
+                waveValue -= randEnemyCost;
+            }
+            else if (waveValue <= 0)
+            {
+                break;
+            }
+        }
+        enemiesToSpawn.Clear();
+        enemiesToSpawn = generatedEnemies;
+    }
+
 }
+
+[System.Serializable]
+public class Enemy
+{
+    public GameObject enemyPrefab;
+    public int cost;
+}
+    //public List<GameObject> enemyList = new List<GameObject>();
+    //public float spawnRangeXMin;
+    //public float spawnRangeXMax;
+    //public float spawnRangeYMin;
+    //public float spawnRangeYMax;
+    //public int numToSpawnPerInterval = 5;
+    //public int SpawnCd;
+
+    //void Start()
+    //{
+    //    StartCoroutine(spawnEnemy(SpawnCd, enemyList));
+    //}
+
+    //private IEnumerator spawnEnemy(float interval, List<GameObject> enemy)
+    //{
+    //    for (int i = 0; i <= numToSpawnPerInterval; i++)
+    //    {
+    //        int selectEnemy = Random.Range(0, enemyList.Count - 1);
+    //        Vector3 v3Pos = Camera.main.ViewportToWorldPoint(new Vector3(Random.Range(spawnRangeXMin, spawnRangeXMax), Random.Range(spawnRangeYMin, spawnRangeYMax), 0.5f));
+    //        v3Pos.z = -0.2f;
+    //        GameObject spawn = Instantiate(enemy[selectEnemy], v3Pos, Quaternion.identity);
+
+    //    }
+    //    yield return new WaitForSeconds(2f);
+    //    yield return new WaitForSeconds(interval);
+    //    StartCoroutine(spawnEnemy(interval, enemy));
+    //}
