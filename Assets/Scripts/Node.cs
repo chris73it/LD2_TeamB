87b2fe2e79b05c56d5d1330249e593bca1d367cf
Adam Mendoza <adammendoza27@gmail.com>
2023-03-02 10:56:22 -0800

    what is this

---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "ScriptableObjects/MapNodes")]
+
+public class Node : ScriptableObject
+{
+    public string nodeType;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
