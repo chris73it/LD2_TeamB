b6460093577515df9aafcb8f833db3cafa11dfff
WildByte <52081661+TheWildByte@users.noreply.github.com>
2023-04-13 11:32:35 -0700

    bungus
    Draggable shop troops work and they're added to the inventory. Just need to instantiate them on the main level.

--- Assets/Scripts/Shop.cs
+++ Assets/Scripts/Shop.cs
@@ -17,6 +17,8 @@ public class Shop : MonoBehaviour
     public ShopItem item1, item2, item3;
     public ShopTroop troop1, troop2, troop3;
 
+    public GameObject draggableTroop, canvas;
+
     private void Start()
     {
         updateText();
@@ -83,6 +85,11 @@ public class Shop : MonoBehaviour
             player.coins.Subtract(shopTroop.troop.getCost());
             player.barracks.addTroop(shopTroop.troop);
             updateText();
+
+            GameObject newDraggableTroop = (GameObject)GameObject.Instantiate(draggableTroop, this.transform.position, this.transform.rotation);
+            newDraggableTroop.GetComponent<DraggableItem>().Setup(shopTroop.troop);
+            newDraggableTroop.transform.SetParent(canvas.transform);
+            newDraggableTroop.transform.localScale = new Vector3(1, 1, 1);
         }
         else
         {
